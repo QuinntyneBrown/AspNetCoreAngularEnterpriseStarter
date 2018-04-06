0f0ca38907be8a85a49c35627f1116221e9371cb
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2018-04-06 10:09:50 -0400

    remove unneeded files etc..

--- src/AspNetCoreAngularEnterpriseStarter.SPA/ClientApp/src/app/shared/shared.module.ts
+++ src/AspNetCoreAngularEnterpriseStarter.SPA/ClientApp/src/app/shared/shared.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OlympicWinner } from './models/olympic-winner.model';
-import { HeaderComponent } from './components/header.component';
 import { HttpClientModule } from '@angular/common/http';
 import { OlympicWinnersService } from './services/olympic-winners.service';
-
-const declarations = [
-  HeaderComponent
-];
 
 const providers = [
   OlympicWinnersService
@@ -18,8 +12,6 @@ const providers = [
     HttpClientModule,
     CommonModule
   ],
-  declarations,
   providers,
-  exports:declarations
 })
 export class SharedModule { }
